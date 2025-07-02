d86cfefbd80daecad551f1f92cdcf67bc1616b5f
balanivetha06 <balanivethab24cse@vetias.ac.in>
2025-07-02 14:18:54 +0530

    my first java programming

--- src/com/vetias/java/workshop/basic/Greetings.java
+++ src/com/vetias/java/workshop/basic/Greetings.java
@@ -1,7 +1,7 @@
     package com.vetias.java.workshop.basics;
     public class Greetings{
      public static void main(String[]args){
-        System.out.println("welcome to java programming")
+        System.out.println("welcome to java programming");
      }
 
 }
