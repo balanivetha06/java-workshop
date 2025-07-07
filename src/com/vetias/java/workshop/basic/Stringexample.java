92dbcb3313f604b306af9ea3890b60047b149d59
balanivetha06 <balanivethab24cse@vetias.ac.in>
2025-07-07 10:27:25 +0530

    string

---
+++ src/com/vetias/java/workshop/basic/Stringexample.java
@@ -0,0 +1,13 @@
+package com.vetias.java.workshop.basic;
+
+public class Stringexample {
+    public static void main(String[]args){
+        String firstname="bala";
+        String lastname="nivetha";
+        String Name=firstname.concat(lastname);
+        System.out.println(Name);
+        String finalname=Name.toUpperCase();
+        System.out.println(finalname);
+    }
+
+}
