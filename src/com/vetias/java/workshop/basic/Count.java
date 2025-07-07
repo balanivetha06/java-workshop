9ab1b228b6f0a04122b7b95d9d35ca8eb0424080
balanivetha06 <balanivethab24cse@vetias.ac.in>
2025-07-07 10:44:08 +0530

    count

---
+++ src/com/vetias/java/workshop/basic/Count.java
@@ -0,0 +1,11 @@
+package com.vetias.java.workshop.basic;
+
+public class Count {
+    public static void main(String[]args){
+        String sentence="This program show how can we split a string into multiple strings";
+        String words[]= sentence.split(" ");
+        int num=words.length;
+        System.out.println(num);
+    }
+
+}
