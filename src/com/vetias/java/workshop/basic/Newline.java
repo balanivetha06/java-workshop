c2e69804d8c38d837ab6545495b991a8fe2680e0
balanivetha06 <balanivethab24cse@vetias.ac.in>
2025-07-07 11:01:59 +0530

    Newline

---
+++ src/com/vetias/java/workshop/basic/Newline.java
@@ -0,0 +1,18 @@
+package com.vetias.java.workshop.basic;
+
+import java.util.Arrays;
+
+public class Newline {
+    public static void main(String[]args){
+        String sentence="This program show how can we split a string into multiple strings";
+        String words[]= sentence.split(" ");
+        for(String word:words)
+        {
+            System.out.println(word);
+        }
+
+    }
+
+    
+
+}
