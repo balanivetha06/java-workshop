64db52bfeecfc9f0b9086f9dcae3ca9d9af68a5b
balanivetha06 <balanivethab24cse@vetias.ac.in>
2025-07-08 10:29:46 +0530

    Agecaluculate

---
+++ src/com/vetias/java/workshop/basic/Agecalculate.java
@@ -0,0 +1,19 @@
+package src.com.vetias.java.workshop.basic;
+import java.time.LocalDate;
+import java.util.Scanner;
+public class Agecalculate {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Date of birth (YYY-MM-DD)):");
+        String dateofBirth=sc.nextLine();
+        LocalDate birthdate=LocalDate.parse(dateofBirth);
+        int yearofbirth=birthdate.getYear();
+        int currentYear=LocalDate.now().getYear();
+        int age= currentYear - yearofbirth;
+        System.out.println("Your age:"+age);
+    
+
+
+    }
+
+}
