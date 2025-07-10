a2556c068b03e9040bf38cb1fbd9670cabc49d42
balanivetha06 <balanivethab24cse@vetias.ac.in>
2025-07-10 15:43:19 +0530

    StudentSet

---
+++ src/main/java/com/vetias/workshop/Collection/StudentSet.java
@@ -0,0 +1,19 @@
+package com.vetias.workshop.Collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StudentSet {
+    public static void main(String[] args){
+        Set<Integer> studentIds = new HashSet<>();
+        studentIds.add(01);
+        studentIds.add(02);
+        studentIds.add(03);
+        studentIds.add(04);
+        studentIds.add(05);
+        System.out.println("Student Ids:"+ studentIds);
+        
+
+    }
+
+}
